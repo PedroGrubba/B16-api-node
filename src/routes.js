acced23a79a1e0960b611a83df003fec573ef9fa
PedroGrubba <pedro.grubba@outlook.com>
2020-07-06 19:53:33 -0300

    feature/tracking-correios

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 const routes = require('express').Router();
+const trackingRoutes = require('./modules/tracking-correios/routes');
 
 routes.get('/', (req, res) => {
     res.send('<h1>Bem-Vindo</h1><h2>O servidor está em execução.</h2>');
 });
 
-//routes.use('/api', ongsRoute);
+routes.use('/api', trackingRoutes);
 
 module.exports = routes;
