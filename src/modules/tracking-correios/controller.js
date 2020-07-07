9e96f753b9b509536e673f38074b326e1f2fbfa8
PedroGrubba <pedro.grubba@outlook.com>
2020-07-06 22:02:24 -0300

    feature/tracking-correios

--- src/modules/tracking-correios/controller.js
+++ src/modules/tracking-correios/controller.js
@@ -1,4 +1,4 @@
-const TrackingCorreios = require('tracking-correios')
+const { rastro } = require('rastrojs');
 
 module.exports = {
     /**
@@ -14,38 +14,14 @@ module.exports = {
                 message: 'Code not found'
             })
         } else {
-            TrackingCorreios.track( code )
-            .then(result => {
-
-                const track = {
-                    numero:     result[0].numero,
-                    sigla:      result[0].sigla, 
-                    nome:       result[0].nome,
-                    categoria:  result[0].categoria,
-                    tipo:       result[0].evento[0].tipo,
-                    status:     result[0].evento[0].status,
-                    data:       result[0].evento[0].data,
-                    hora:       result[0].evento[0].hora,
-                    descricao:  result[0].evento[0].descricao,
-                    local:      result[0].evento[0].local,
-                    codigoLocal: result[0].evento[0].codigo,
-                    cidade:     result[0].evento[0].cidade,
-                    uf:         result[0].evento[0].uf
-                };
-
+            const result = await rastro.track( code );
+            if(result){
                 res.send({
-                    success: true,
-                    track
+                    sucess: true,
+                    result
                 });
-            })
-            .catch(err => {
-                res.status(500).send({
-                    success: false,
-                    error: err.message
-                });
-            });
+            }
         }
-
     },
     
 };
